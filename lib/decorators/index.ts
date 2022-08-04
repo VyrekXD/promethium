8eca812cebcdbbf4f7e11d2d9154067bd683e22c
Vyrek <65917996+VyrekXD@users.noreply.github.com>
2022-08-04 18:01:22 -0500

    Delete lib/decorators directory

--- lib/decorators/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './metadata.js'
-export * from './is-private.js'
-export * from './command-options.js'
--- lib/decorators/is-private.ts
+++
@@ -1,9 +0,0 @@
-import { Collections } from 'detritus-client'
-
-export function IsPrivate(guildsIds: string[]) {
-	return function (target: any): any {
-		return class extends target {
-			guildIds = new Collections.BaseSet([...guildsIds])
-		}
-	}
-}
